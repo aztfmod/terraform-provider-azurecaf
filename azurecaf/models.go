9e9d9fe6f6862bb600311c4a50aac77ea71f972b
Abdullah Khairi Kamarul Zaman <v-abkama@microsoft.com>
2020-04-15 14:10:06 +0000

    add agw, apim, appi resources and added invappi filter regex temporary to filter custom invalid char

--- azurecaf/models.go
+++ azurecaf/models.go
@@ -12,13 +12,16 @@ const (
 )
 
 const (
-	alphanum            string = "[^0-9A-Za-z]"
-	alphanumh           string = "[^0-9A-Za-z,-]"
-	alphanumu           string = "[^0-9A-Za-z,_]"
-	alphanumhu          string = "[^0-9A-Za-z,_,-]"
-	alphanumhup         string = "[^0-9A-Za-z,_,.,-]"
-	alphanumStartletter string = "\\A[^a-z][^0-9A-Za-z]"
-	unicode             string = `[^-\w\._\(\)]`
+	alphanum    string = "[^0-9A-Za-z]"
+	alphanumh   string = "[^0-9A-Za-z,-]"
+	alphanumu   string = "[^0-9A-Za-z,_]"
+	alphanumhu  string = "[^0-9A-Za-z,_,-]"
+	alphanumhup string = "[^0-9A-Za-z,_,.,-]"
+	unicode     string = `[^-\w\._\(\)]`
+	invappi     string = "[%&\\?/]" //appinisghts invalid character
+
+	//Need to find a way to filter beginning and end of string
+	//alphanumstartletter string = "\\A[^a-z][^0-9A-Za-z]"
 )
 
 const (
@@ -48,6 +51,9 @@ var Resources = map[string]ResourceStructure{
 	"aaa":  {"azure automation account", "aaa", 6, 50, false, alphanumh, "^[a-zA-Z][0-9A-Za-z,-]{5,49}$"},
 	"acr":  {"azure container registry", "acr", 5, 49, true, alphanum, "^[0-9A-Za-z]{5,50}$"},
 	"afw":  {"azure firewall", "afw", 1, 80, false, alphanumhup, "^[a-zA-Z][0-9A-Za-z,_,.,-]{0,79}$"},
+	"agw":  {"application gateway", "agw", 1, 80, false, alphanumhup, "^[0-9a-zA-Z][0-9A-Za-z,_,.,-]{0,79}[0-9a-zA-Z,_]{0,1}$"},
+	"apim": {"api management", "apim", 1, 50, false, alphanum, "^[a-zA-Z][0-9A-Za-z,-]{0,49}$"},
+	"appi": {"application insights", "appi", 1, 260, false, invappi, "^[a-zA-Z][0-9A-Za-z,-]{0,49}$"},
 	"asr":  {"azure site recovery", "asr", 2, 50, false, alphanumh, "^[a-zA-Z][0-9A-Za-z,-]{1,49}$"},
 	"evh":  {"event hub", "evh", 1, 50, false, alphanumh, "^[a-zA-Z][0-9A-Za-z,-]{0,48}[0-9a-zA-Z]{0,1}$"},
 	"gen":  {"generic", "gen", 1, 24, false, alphanum, "^[0-9a-zA-Z]{1,24}$"},
@@ -61,7 +67,7 @@ var Resources = map[string]ResourceStructure{
 	"st":   {"storage account", "st", 3, 24, true, alphanum, "^[0-9a-z]{3,24}$"},
 	"vml":  {"virtual machine (linux)", "vml", 1, 64, false, alphanumh, "^[0-9a-zA-Z][0-9A-Za-z,_,-]{0,63}[0-9a-zA-Z,_]{0,1}$"},
 	"vmw":  {"virtual machine (windows)", "vmw", 1, 15, false, alphanumh, "^[0-9a-zA-Z][0-9A-Za-z,_,-]{0,13}[0-9a-zA-Z,_]{0,1}$"},
-	"vnet": {"virtual network", "vnet", 1, 64, false, alphanumhup, "^[0-9a-zA-Z][0-9A-Za-z,_,.,-]{0,62}[0-9a-zA-Z,_]{0,1}$"},
+	"vnet": {"virtual network", "vnet", 2, 64, false, alphanumhup, "^[0-9a-zA-Z][0-9A-Za-z,_,.,-]{0,62}[0-9a-zA-Z,_]{0,1}$"},
 }
 
 // ResourcesMapping enforcing new naming convention
