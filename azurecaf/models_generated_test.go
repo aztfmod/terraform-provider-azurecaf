39a7f59822fad2207b3cb66ff16643255178360d
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2020-07-15 20:39:49 +0100

    correct regex and add regex tests

---
+++ azurecaf/models_generated_test.go
@@ -0,0 +1,16 @@
+package azurecaf
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestCompileRegexValidation(t *testing.T) {
+	for _, resource := range ResourceDefinitions {
+		_, err := regexp.Compile(resource.ValidationRegExp)
+		if err != nil {
+			t.Logf("Error on the regex %s for the resource %s error %v", resource.ValidationRegExp, resource.ResourceTypeName, err.Error())
+			t.Fail()
+		}
+	}
+}
