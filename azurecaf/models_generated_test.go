6b1258f75871d3c1707a1276ba25a89bd9a123fe
Nicolas Yuen <nicolas.yuen@gmail.com>
2020-07-17 17:48:19 +0800

    updating clening regex

--- azurecaf/models_generated_test.go
+++ azurecaf/models_generated_test.go
@@ -9,7 +9,12 @@ func TestCompileRegexValidation(t *testing.T) {
 	for _, resource := range ResourceDefinitions {
 		_, err := regexp.Compile(resource.ValidationRegExp)
 		if err != nil {
-			t.Logf("Error on the regex %s for the resource %s error %v", resource.ValidationRegExp, resource.ResourceTypeName, err.Error())
+			t.Logf("Error on the validation regex %s for the resource %s error %v", resource.ValidationRegExp, resource.ResourceTypeName, err.Error())
+			t.Fail()
+		}
+		_, err = regexp.Compile(resource.RegEx)
+		if err != nil {
+			t.Logf("Error on the regex %s for the resource %s error %v", resource.RegEx, resource.ResourceTypeName, err.Error())
 			t.Fail()
 		}
 	}
